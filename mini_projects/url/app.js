ed2f07a46ade86d39e269527b3a7683b7608f1fa
SaniyaArora03 <saniyaarorapdav@gmail.com>
2025-07-02 14:22:57 +0530

    server

--- mini_projects/url/app.js
+++ mini_projects/url/app.js
@@ -100,6 +100,7 @@ const server = createServer(async (req, res) => {
 });
 
 // Start the server
-server.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+server.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
+
